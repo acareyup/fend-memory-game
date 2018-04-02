684e92a20000e66e256cbe6006315188a760b74b
Eyup-Acar <acareyup@gmail.com>
2018-04-02 19:46:34 +0300

    create list of icons in the array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+let iconArray = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", 
+    "fa fa-bolt", "fa fa-bolt", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-cube", "fa fa-cube", "fa fa-bomb", "fa fa-bomb"]
 
 /*
  * Display the cards on the page
